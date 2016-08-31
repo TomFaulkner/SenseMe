b2101292654dd166dc576df54bbbf2933e842cb7
Tomf80 <Tom@tom.am>
2016-08-30 20:33:05 -0500

    Updated to match example on read me
    Currently does nothing, uncomment to try things.

--- run.py
+++ run.py
@@ -1,37 +1,38 @@
-import time
-
 from sensemefan import SenseMeFan
 
-# TODO: Function to broadcast the <ALL;DEVICE;ID;GET> command to find the fan
-# TODO: Get IP from the above, since it isn't in the response
-# TODO: Fix fan_listen to actually listen for broadcasts
-
 def main():
+    # Statically assign the fan? Probably not, but you would do it this way:
     # fan = SenseMeFan('192.168.1.112', 'Living Room Fan')
     fan = SenseMeFan()
 
+    # Turn the light off
     # fan.lightoff()
+
+    # Get Light level
     # light = fan.getlight()
     # print(light)
 
+    # Get fan speed
     # motor = fan.getfan()
     # print(motor)
 
+    # Toggle light status
     # fan.lighttoggle()
 
+    # want an increasing light effect? Do this.
+    # But, really, probably don't, I don't think they intended strobe effects.
+    # I'm not responsible if you make a strobe light and break the fan or worse
     # for intensity in range(1,16):
     #   fan.setlight(intensity)
     #   time.sleep(1)
 
+    # Increase light level by 2 levels
     # fan.inclight(2)
 
-    # findfan()
-
-    # print(fan.__query__('<ALL;DEVICE;ID;GET>'))
-
+    # Listen for broadcasts, useful for debugging,
+    # wouldn't suggest using it for anything else
     # fan.listen()
 
-    # print(fan.fan_listen())
     return
 
 main()
