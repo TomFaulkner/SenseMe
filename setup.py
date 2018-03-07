d04ef7869bcd7386462662d83b378ff6681c6bd8
Tom Faulkner <tomfaulkner@gmail.com>
2018-03-06 21:23:58 -0600

    more setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,27 @@
 
 from distutils.core import setup
 
+long_description = """HaikuHome SenseMe API for fans and lights.
+
+A simple library to automate control and monitoring of network connected
+ HaikuHome SenseMe devices.
+
+Python 3.6 or above is required.
+"""
+
 setup(name='SenseMe',
-      version='1.0',
-      description='Haiu Home SenseMe API',
+      version='0.1.0',
+      description='HaikuHome SenseMe API for fans and lights',
       author='Tom Faulkner',
       author_email='tomfaulkner@gmail.com',
       url='https://github.com/TomFaulkner/SenseMe',
-      packages=['senseme', 'senseme.lib'])
+      packages=['senseme', 'senseme.lib'],
+      license='GPL3',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+      ],
+      keywords='HaikuHome SenseMe fan light home automation bigassfans',
+      python_requires='>=3.6')
