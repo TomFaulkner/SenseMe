0c772e9a613c70111298b6c5141e943cba067610
Tom Faulkner <tomfaulkner@gmail.com>
2018-03-06 21:09:19 -0600

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='SenseMe',
+      version='1.0',
+      description='Haiu Home SenseMe API',
+      author='Tom Faulkner',
+      author_email='tomfaulkner@gmail.com',
+      url='https://github.com/TomFaulkner/SenseMe',
+      packages=['senseme', 'senseme.lib'])
