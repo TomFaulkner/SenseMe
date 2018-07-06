bae0f3231ed119ffca813534c711239e6c696381
Tom Faulkner <tomfaulkner@gmail.com>
2018-07-05 22:08:09 -0500

    add script, update setup.py, remove symlink, release 0.1.3

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 long_description = """HaikuHome SenseMe API for fans and lights.
 
@@ -12,19 +12,21 @@ Python 3.6 or above is required.
 
 setup(
     name="SenseMe",
-    version="0.1.2",
+    version="0.1.3",
     description="HaikuHome SenseMe API for fans and lights",
     author="Tom Faulkner",
     author_email="tomfaulkner@gmail.com",
     url="https://github.com/TomFaulkner/SenseMe",
-    packages=["senseme", "senseme.lib"],
+    packages=["senseme", "senseme.lib", "bin"],
     license="GPL3",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Topic :: Home Automation"
     ],
     keywords="HaikuHome SenseMe fan light home automation bigassfans",
     python_requires=">=3.6",
+    scripts=['bin/senseme_cli']
 )
